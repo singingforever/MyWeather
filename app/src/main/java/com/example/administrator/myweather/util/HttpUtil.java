faba5a9e70dc60bfe10d39dea8eb2fdcee2e05f5
singingforever <Singingfor@163.com>
2016-03-18 23:01:52 +0800

    解析服务器数据
    P498，还未设置布局。

--- app/src/main/java/com/example/administrator/myweather/util/HttpUtil.java
+++ app/src/main/java/com/example/administrator/myweather/util/HttpUtil.java
@@ -1,6 +1,8 @@
 package com.example.administrator.myweather.util;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -20,10 +22,24 @@ public class HttpUtil {
                     connection.setConnectTimeout(8000);
                     connection.setReadTimeout(8000);
                     InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    if (listener != null) {
+                        listener.onFinish(response.toString());
+                    }
 
                 }
                 catch (Exception e) {
-                    e.printStackTrace();
+                    listener.onError(e);
+                }
+                finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
                 }
 
             }
---
+++ app/src/main/java/com/example/administrator/myweather/util/Utility.java
@@ -0,0 +1,70 @@
+package com.example.administrator.myweather.util;
+
+import android.text.TextUtils;
+
+import com.example.administrator.myweather.model.City;
+import com.example.administrator.myweather.model.County;
+import com.example.administrator.myweather.model.MyWeatherDB;
+import com.example.administrator.myweather.model.Province;
+
+/**
+ * Created by Administrator on 2016/3/18.
+ */
+public class Utility {
+    //解析和处理服务器返回的省级数据
+    public synchronized  static boolean handleProvincesResponse(MyWeatherDB myWeatherDB, String response) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allProvinces = response.split(",");
+            if (allProvinces != null && allProvinces.length > 0) {
+                for (String p : allProvinces) {
+                    String[] array = p.split("\\|");
+                    Province province = new Province();
+                    province.setProvinceCode(array[0]);
+                    province.setProvinceName(array[1]);
+                    //讲解析出来的数据存储到province表
+                    myWeatherDB.saveProvince(province);
+                }
+            }
+        }
+        return false;
+    }
+
+    //解析和处理服务器返回的市级数据
+    public  static boolean handleCitiesResponse(MyWeatherDB myWeatherDB, String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCities = response.split(",");
+            if (allCities != null && allCities.length > 0) {
+                for (String c : allCities) {
+                    String[] array = c.split("\\|");
+                    City city = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+                    //将解析出来的数据存储到City表
+                    myWeatherDB.saveCity(city);
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountiesResponse(MyWeatherDB myWeatherDB, String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCounties = response.split(",");
+            if (allCounties != null && allCounties.length > 0) {
+                for (String c : allCounties) {
+                    String[] array = c.split(",");
+                    County county = new County();
+                    county.setCountyCode(array[0]);
+                    county.setCountyName(array[1]);
+                    county.setId(cityId);
+                    //将解析出来的数据存储到County表
+                    myWeatherDB.saveCounty(county);
+                }
+            }
+        }
+        return false;
+    }
+
+}
